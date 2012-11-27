11b6eb2371e4630a791c771069fe6dafaca8c1af
Frantisek Mantlik <frantisek@mantlik.cz>
2012-11-27 11:20:16 +0100

    Update script changed to work when branch has been switched.

--- update_vendor.bat
+++ update_vendor.bat
@@ -1,16 +1,2 @@
-call git checkout cssbox-vendor
-call git svn rebase
-
-call git checkout jstyleparser-vendor
-call git svn rebase
-
-call git checkout swingbox-vendor
-call git svn rebase
-
-call git checkout master
-
-call git merge --no-ff -s subtree cssbox-vendor
-call git merge --no-ff -s subtree jstyleparser-vendor
-call git merge --no-ff -s subtree swingbox-vendor
-
-pause
+cp update_vendor_helper.bat %TEMP%\update_vendor_helper.bat
+%TEMP%\update_vendor_helper.bat
