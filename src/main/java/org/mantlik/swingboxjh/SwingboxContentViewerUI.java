072b9411283f1535612680a51a4450f1decc3df2
Frantisek Mantlik <frantisek@mantlik.cz>
2012-12-10 21:37:08 +0100

    Updated license information and project hosting details for code.google.com.

--- src/main/java/org/mantlik/swingboxjh/SwingboxContentViewerUI.java
+++ src/main/java/org/mantlik/swingboxjh/SwingboxContentViewerUI.java
@@ -1,3 +1,24 @@
+/*
+ * #%L
+ * swingbox-javahelp-viewer
+ * %%
+ * Copyright (C) 2012 Frantisek Mantlik <frantisek at mantlik.cz>
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
 package org.mantlik.swingboxjh;
 
 import java.awt.BorderLayout;
@@ -40,13 +61,8 @@ import javax.swing.text.Highlighter;
 import javax.swing.text.Highlighter.HighlightPainter;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
-import org.fit.cssbox.demo.DOMSource;
 import org.fit.cssbox.swingbox.BrowserPane;
 import org.openide.util.Exceptions;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Text;
-import org.xml.sax.SAXException;
 
 /**
  * Javahelp component viewer based on SwingBox viewer
